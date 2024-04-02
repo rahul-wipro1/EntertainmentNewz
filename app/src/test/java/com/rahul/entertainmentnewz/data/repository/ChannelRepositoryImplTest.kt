bab140da19500f85baf2a8954b41f7712109ca16
rahul-wipro <rahul.khamitkar@wipro.com>
2024-04-02 11:40:53 +0530

    Adding MockK lib and unit test cases

---
+++ app/src/test/java/com/rahul/entertainmentnewz/data/repository/ChannelRepositoryImplTest.kt
@@ -0,0 +1,61 @@
+package com.rahul.entertainmentnewz.data.repository
+
+import com.rahul.entertainmentnewz.MainDispatcherRule
+import com.rahul.entertainmentnewz.data.datasource.NetworkApi
+import com.rahul.entertainmentnewz.data.dto.ChannelsDTOItem
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ChannelRepositoryImplTest {
+    private lateinit var networkApi: NetworkApi
+    private lateinit var channelRepository: ChannelRepositoryImpl
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        networkApi = mockk()
+        channelRepository = ChannelRepositoryImpl(networkApi)
+    }
+
+    @After
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun getAllChannels() = runTest {
+        val mockList = listOf(ChannelsDTOItem("11-11-2023", "disc", "icon", "pic", "title"))
+
+        coEvery { networkApi.getChannelsList() } returns mockList
+
+        val result = channelRepository.getAllChannels()
+
+        assertEquals(mockList, result)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun `getAllChannels with Empty response`() = runTest {
+
+        coEvery { networkApi.getChannelsList() } returns emptyList()
+
+        val result = channelRepository.getAllChannels()
+
+        Assert.assertTrue(result.isEmpty())
+    }
+
+}
---
+++ app/src/test/java/com/rahul/entertainmentnewz/domain/usecase/ChannelListUseCaseTest.kt
@@ -0,0 +1,71 @@
+package com.rahul.entertainmentnewz.domain.usecase
+
+import com.rahul.entertainmentnewz.MainDispatcherRule
+import com.rahul.entertainmentnewz.data.dto.ChannelsDTOItem
+import com.rahul.entertainmentnewz.domain.model.ChannelListItem
+import com.rahul.entertainmentnewz.domain.repository.ChannelRepository
+import com.rahul.entertainmentnewz.utils.ResponseState
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import java.io.IOException
+
+class ChannelListUseCaseTest {
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    private lateinit var channelRepository: ChannelRepository
+    private lateinit var channelListUseCase: ChannelListUseCase
+
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+        channelRepository = mockk()
+        channelListUseCase = ChannelListUseCase(channelRepository)
+    }
+
+    @After
+    fun tearDown() {
+        clearAllMocks()
+    }
+
+    @Test
+    fun `invoke loading by success`() = runTest{
+        coEvery { channelRepository.getAllChannels()} returns listOf(ChannelsDTOItem("11-11-2023","disc","icon","pic","title"))
+
+        val resultFlow = channelListUseCase()
+
+        val result = mutableListOf<ResponseState<List<ChannelListItem>>>()
+        resultFlow.collect{
+            result.add(it)
+        }
+        Assert.assertTrue(result[0] is ResponseState.Loading)
+        Assert.assertTrue(result[1] is ResponseState.Success)
+        val successState =  result[1] as ResponseState.Success
+        Assert.assertEquals(1,successState.data?.size)
+    }
+
+    @Test
+    fun `invoke loading by error`() = runTest{
+        coEvery { channelRepository.getAllChannels()} throws IOException()
+        val resultFlow = channelListUseCase()
+
+        val result = mutableListOf<ResponseState<List<ChannelListItem>>>()
+        resultFlow.collect{
+            result.add(it)
+        }
+        Assert.assertTrue(result[0] is ResponseState.Loading)
+        Assert.assertTrue(result[1] is ResponseState.Error)
+        val errorState =  result[1] as ResponseState.Error
+        Assert.assertEquals("Error Occurred",errorState.message)
+    }
+}
